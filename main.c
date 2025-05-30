c8a8ab41aa5168b2377b65096f01fa7b3f6efc5a
andrew-malokhatko <andrew.malokhatko@gmail.com>
2025-05-30 18:50:00 +0200

    Idk

--- main.c
+++ main.c
@@ -60,6 +60,7 @@ int main(int argc, char* argv[])
     // struct sockaddr_in server_addr;   
     int listener_fd;
     struct addrinfo hints, *res;
+    struct sockaddr_storage client_addr;
     //int addrlen = sizeof(server_addr);
 
     memset(&hints, 0, sizeof(hints));
@@ -71,27 +72,70 @@ int main(int argc, char* argv[])
     sprintf(portStr, "%d", PORT);
 
     int error;
-    if ((error = getaddrinfo("localhost", portStr, &hints, &res)) != 0)
+    if ((error = getaddrinfo(NULL, portStr, &hints, &res)) != 0)
     {
         perror("Watafuck: ");
         exit(1);
     }
 
-    for (struct addrinfo *p = res; p != NULL; p = p->ai_next)
+    // Create socket and bind it to the address
+    int yes = 1;
+    struct addrinfo *it = res;
+
+    for (; it != NULL; it = it->ai_next)
     {
-        listener_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        listener_fd = socket(it->ai_family, it->ai_socktype, it->ai_protocol);
         if (listener_fd < 0)
         {
-            perror("Socket creation failed");
             continue;
         }
 
-        if (bind(listener_fd, p->ai_addr, p->ai_addrlen) < 0)
+        setsockopt(listener_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+
+        if (bind(listener_fd, it->ai_addr, it->ai_addrlen) < 0)
         {
             close(listener_fd);
             continue;
         }
+
+        break; // successfully bound
     }
+
+    if (it == NULL)
+    {
+        fprintf(stderr, "Failed to bind socket\n");
+        freeaddrinfo(res);
+        exit(2);
+    }
+
+    // listen for incoming connections
+    if (listen(listener_fd, 10) == -1)
+    {
+        perror("Listen failed");
+        close(listener_fd);
+        freeaddrinfo(res);
+        exit(1);
+    }
+
+    // accept a connection
+    socklen_t addr_len = sizeof(client_addr);
+    int client_fd = accept(listener_fd, (struct sockaddr*)&client_addr, &addr_len);
+
+    const char *http_response =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/html\r\n"
+            "Connection: close\r\n"
+            "\r\n"
+            "<html><body><h1>Hello from C Web Server!</h1></body></html>\r\n";
+
+    // Send the http response to the client
+    write(client_fd, http_response, strlen(http_response));
+    printf("Client connected. Data sent\n");
+    close(client_fd);
+    printf("Client disconnected.\n");
+    
+    close(listener_fd);
+    freeaddrinfo(res);
     
     return 0;
 }
