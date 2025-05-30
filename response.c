f9c06e75d6483449758e4f2c405c430042f8b470
JeniaD <jeniadeshchenko@gmail.com>
2025-05-30 19:05:04 +0200

    fix

--- response.c
+++ response.c
@@ -1,7 +1,8 @@
+#include <stdlib.h>
 #include "config.h"
 
-void formulateResponse(char** response, char* body){
+void formulateResponse(char** response, int status, char* body){
     *response = malloc(RESPMAXSIZE);
     int contentSize = sizeof(body);
-    
+    snprintf(*response, sizeof(*response), "HTTP/1.1 %d", status);
 }
